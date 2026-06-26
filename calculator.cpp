91a5e963321d4adb96c60c9ccca7c2fa78e1a1a2
MrNaakka <herman.virtanen@gmail.com>
2026-06-26 23:58:16 +0300

    Created a calculator to further test functionality and play around.

---
+++ calculator.cpp
@@ -0,0 +1,54 @@
+
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include "adder.hpp"
+#include "gates.hpp"
+#include "alu.hpp"
+
+void calculator() {
+  uint32_t num1;
+  uint32_t num2;
+  std::string oper;
+  bool play = true;
+  while (play) {
+    std::cout << "enter first num: ";
+    std::cin >> num1;
+
+    std::cout << "enter operator (+ or -): ";
+    std::cin >> oper;
+
+    std::cout << "enter second num: ";
+    std::cin >> num2;
+
+    Bus in1{};
+    Bus in2{};
+
+    for (int i = 0; i < WORD; i++) {
+      in1[i] = {.value = static_cast<bool>((num1 >> i) & 1)};
+      in2[i] = {.value = static_cast<bool>((num2 >> i) & 1)};
+    }
+
+    Gate isSubstrac{.value = false};
+    if (oper == "-") {
+      isSubstrac.value = true;
+    }
+
+    auto result = addBus(in1, in2, isSubstrac);
+    std::cout << "here is the result num: " << result.result << std::endl;
+    
+    auto aluRes = aluBus(in1, in2, {.value = false}, isSubstrac, {Gate{.value = false}, Gate{.value = true}});
+    std::cout << "here is the alu num: " << aluRes.result << std::endl;
+
+    std::string cont;
+    std::cout << "do you want to contineu? y/n: ";
+    std::cin >> cont;
+
+    if (cont == "n") {
+      play = false;
+    }
+  }
+}
