72f070bdd4c3e3b0e55e7045dfba9e29e28ffd3e
MrNaakka <herman.virtanen@gmail.com>
2026-07-14 22:32:39 +0300

    Implemnted counter.

---
+++ sequintial/counter.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "../combinational/adder.hpp"
+#include "../combinational/gates.hpp"
+#include "memory.hpp"
+
+struct Counter {
+private:
+  Register r;
+
+public:
+  std::array<Gate, 2> selectorSignal; // increment, load, reset, empty
+  Bus in;
+
+  Bus read() const { return r.read(); }
+
+  void tick() {
+    Bus addOne{};
+    addOne[0] = {true};
+    Bus current = r.read();
+    Bus incrementRes = addBus(current, addOne, {false}).result;
+
+    Bus loadRes = in;
+
+    Bus resetRes {};
+
+    std::array<Bus, 3> muxIn {incrementRes, loadRes, resetRes};
+    Bus newIn = muxBusNTo1(muxIn, selectorSignal);
+    r.in = newIn;
+    r.load = {true};
+    r.tick();
+  }
+};
