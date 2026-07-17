f5bb648cdca2529b2e18e5675b220298684667dd
MrNaakka <herman.virtanen@gmail.com>
2026-07-17 17:30:35 +0300

    Combinational logic for the result of load and load-high operation.

---
+++ combinational/load.hpp
@@ -0,0 +1,20 @@
+#include "gates.hpp"
+#include <array>
+
+inline Bus load(const Bus &instruction) {
+  Bus res{};
+  for (int i = 0; i < 16; ++i) {
+    res[i] = instruction[i];
+  }
+  return res;
+}
+
+inline Bus loadHigh(const Bus &instruction,
+                    const Bus &currentOutputRegisterValue) {
+  Bus res{};
+  for (int i = 0; i < 16; ++i) {
+    res[i] = currentOutputRegisterValue[i];
+    res[i + 16] = instruction[i];
+  }
+  return res;
+}
